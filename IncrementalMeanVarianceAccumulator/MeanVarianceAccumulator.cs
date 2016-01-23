4a8c0465e88b993eb3dfaa05637da4f17582a6af
Eamon Nerbonne <eamon@nerbonne.org>
2016-01-23 15:27:42 +0100

    Improve method name

--- IncrementalMeanVarianceAccumulator/MeanVarianceAccumulator.cs
+++ IncrementalMeanVarianceAccumulator/MeanVarianceAccumulator.cs
@@ -34,7 +34,7 @@ namespace IncrementalMeanVarianceAccumulator
 		public double SampleStandardDeviation => Math.Sqrt(SampleVariance);
 		public double Weight => weightSum;
 
-		public static MeanVarianceAccumulator For(IEnumerable<double> vals) => vals.Aggregate(new MeanVarianceAccumulator(), (mv, v) => mv.Add(v));
+		public static MeanVarianceAccumulator FromEnumerable(IEnumerable<double> vals) => vals.Aggregate(new MeanVarianceAccumulator(), (mv, v) => mv.Add(v));
         public static MeanVarianceAccumulator Init(double firstValue, double firstWeight = 1.0) => new MeanVarianceAccumulator(firstWeight, 0.0, firstValue); //equivalent to adding to an empty distribution.
         public static MeanVarianceAccumulator Empty => default(MeanVarianceAccumulator);
 
