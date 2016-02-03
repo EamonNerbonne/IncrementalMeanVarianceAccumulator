0ad2dc7544b8c65dd117a99aa843389151c8d019
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-03 22:30:09 +0100

    Add nuspec file

--- IncrementalMeanVarianceAccumulator/Properties/AssemblyInfo.cs
+++ IncrementalMeanVarianceAccumulator/Properties/AssemblyInfo.cs
@@ -1,18 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IncrementalMeanVarianceAccumulator")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Incrementally compute the variance, mean and count of a sequence in one pass.")]
+[assembly: AssemblyCompany("Eamon Nerbonne")]
 [assembly: AssemblyProduct("IncrementalMeanVarianceAccumulator")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Eamon Nerbonne 2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,3 +17,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5da24550-1be3-417d-9074-77bb1159a9c7")]
+[assembly: AssemblyVersion("0.0.1")]
